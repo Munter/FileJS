02f6ee1fbe44190fc16f94cc8f746747c6f6ff3d
Peter Müller <munter@fumle.dk>
2013-03-05 23:57:43 +0100

    Simplified file input module

--- input.js
+++ input.js
@@ -1,17 +1,10 @@
 define(function () {
     return function (fileInput, callback, fallback) {
         fileInput.onchange = function (e) {
-            e = e || window.event;
-            var target = e.target || e.srcElement,
-                files = e.target && e.target.files,
-                i;
-
-            if (files) {
-                for (i = 0; i < files.length; i += 1) {
-                    callback(files[i]);
-                }
+            if (fileInput.files) {
+                Array.prototype.forEach.call(fileInput.files, callback);
             } else if (fallback) {
-                fallback(e);
+                fallback(fileInput);
             }
         };
     };
