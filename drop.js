2b8a774c5ce7251f383b5cfc7de35a46464eb223
Peter Müller <munter@fumle.dk>
2013-03-20 15:22:22 +0100

    Added more configuration options

--- drop.js
+++ drop.js
@@ -5,13 +5,47 @@ define(function (fileapi) {
             e.dataTransfer.dropEffect = 'copy';
         };
 
-    return function (element, callback) {
+    return function (element, config) {
+        var timer;
+
         if (window.File && window.FileList) {
-            element.addEventListener('dragover', cancelEvent, false);
-            element.addEventListener('dragenter', cancelEvent, false);
+            element.addEventListener('dragover', function (e) {
+                cancelEvent(e);
+
+                if (typeof config.over === 'function') {
+                    config.over(e);
+                }
+            }, false);
+
+            element.addEventListener('dragenter', function (e) {
+                cancelEvent(e);
+
+                timer = setTimeout(function () {
+                    if (typeof config.enter === 'function') {
+                        config.enter(e);
+                    }
+                });
+            }, false);
+
+            element.addEventListener('dragleave', function (e) {
+                cancelEvent(e);
+
+                if (timer) {
+                    clearTimeout(timer);
+                    timer = undefined;
+                } else {
+                    if (typeof config.leave === 'function') {
+                        config.leave(e);
+                    }
+                }
+            }, false);
+
             element.addEventListener('drop', function (e) {
                 cancelEvent(e);
-                Array.prototype.forEach.call(e.dataTransfer.files, callback);
+
+                if (typeof config.drop === 'function') {
+                    Array.prototype.forEach.call(e.dataTransfer.files, config.drop);
+                }
             }, false);
         }
     };
